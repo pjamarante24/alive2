15ffa13a8f2d6cd064d1ea786797b3bb571137bb
Pablo Amarante <p.j.amarante24@gmail.com>
2021-08-05 23:45:28 -0400

    statistics

--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -7,13 +7,17 @@ public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
     public GameManager gameManager;
+    public ZombieManager zombieManager;
     public AudioMixer audioMixer;
     public Text roundText;
+    public Text zombiesLeftText;
+    public Text zombiesKilledText;
     private bool isPaused = false;
 
     private void Start()
     {
         if (!gameManager) gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        if (!zombieManager) zombieManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<ZombieManager>();
     }
 
     private void Update()
@@ -48,6 +52,21 @@ public class PauseMenu : MonoBehaviour
     void UpdateUI()
     {
         roundText.text = gameManager.round.ToString();
+        zombiesLeftText.text = zombieManager.zombiesLeft().ToString();
+        zombiesKilledText.text = CalculateZombiesKilled(gameManager.round, zombieManager.initialZombieCount, zombieManager.zombiesLeft()).ToString();
+    }
+
+    float CalculateZombiesKilled(int round, int initialZombieCount, int zombiesLeft)
+    {
+        float a = initialZombieCount;
+        float d = ((a * 2) - a);
+        float n = round;
+
+        float totalZombies = (n / 2) * ((2 * a) + (n - 1) * d);
+
+        Debug.Log((n / 2) + " * ((" + (2 * a) + ")" + ((n - 1) * d) + ") = " + totalZombies);
+
+        return totalZombies - zombiesLeft;
     }
 
     public void OnExit()
